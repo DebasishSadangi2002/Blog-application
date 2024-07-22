78d5d2e647e941a32fce18a66afb4c6bf272ab27
DebasishSadangi2002 <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-07-22 17:46:44 +0530

    deployment

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://production_mx0x_user:4suWhpBaGevMrR5JswkF61kYoVRbRU3L@dpg-cqf3ldpu0jms739nmip0-a/production_mx0x')
 }
 
 
@@ -122,6 +120,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
